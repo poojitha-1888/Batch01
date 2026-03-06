af674fad699b0f573a55fab63918c69f2ddd956a
poojitha-1888 <jakkampudipoojitha9@gmail.com>
2026-03-06 21:56:29 +0530

    assignment

---
+++ JS/assignment 4/1.js
@@ -0,0 +1,61 @@
+// Student Object
+let student1 = {
+  id: 1,
+  name: "Rahul",
+  age: 20,
+  course: "Computer Science",
+  marks: [85, 90, 88],
+  address: "New York",
+  email: "rahul@email.com"
+};
+
+// Bus Object
+let bus = {
+  busNumber: "B101",
+  driverName: "John",
+  route: "City Center - Airport",
+  capacity: 50,
+  currentPassengers: 35,
+  isAC: true
+};
+
+// Employee Object
+let employee = {
+  empId: 1001,
+  name: "Sarah",
+  department: "IT",
+  salary: 50000,
+  experience: 5,
+  email: "sarah@company.com"
+};
+
+// Mobile Object
+let mobile = {
+  brand: "Samsung",
+  model: "Galaxy S23",
+  price: 900,
+  ram: "8GB",
+  storage: "256GB",
+  battery: "4500mAh",
+  camera: "50MP"
+};
+
+console.log(student1);
+console.log(bus);
+console.log(employee);
+console.log(mobile);
+
+
+
+
+let student2 = {
+  rollNumber: 101,
+  name: "Arjun",
+  marks: [80, 85, 90, 75],
+  address: "Mumbai"
+};
+
+console.log("Roll Number:", student2.rollNumber);
+console.log("Name:", student2.name);
+console.log("Marks:", student2.marks);
+console.log("Address:", student2.address);
